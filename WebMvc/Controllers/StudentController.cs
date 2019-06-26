bdcf35bb0a5387a25bf1fabb5ca6bfeb8fc9d747
赵天明 <@DESKTOP-KBFDARK>
2019-06-26 10:11:36 +0800

    添加项目文件。

---
+++ WebMvc/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Upt(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+        public ActionResult Show()
+        {
+            return View();
+        }
+    }
+}
